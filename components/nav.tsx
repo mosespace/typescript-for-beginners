796becb3dab5ab405849d344ffd4ea3bb08d601d
mosespace <kisakyemoses50@gmail.com>
2024-03-22 12:16:50 +0300

    initial commit

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,15 +1,22 @@
+import { Menu } from "lucide-react";
 import React from "react";
 
 export default function Nav() {
   return (
-    <div className='flex max-w-7xl text-slate-950 bg-white justify-between'>
+    <div className='flex py-2 max-w-7xl text-slate-950 bg-white justify-between'>
       <div>
         <img src='/logo.png' alt='MailChamp' />
       </div>
-      <div>
-        <button>Start Free Trial</button>
-        <button>Login</button>
-        <button>Menu</button>
+      <div className='flex space-x-4'>
+        <button className='bg-[#FFE01B] border border-black border-2 px-4 py-2 rounded-full'>
+          Start Free Trial
+        </button>
+        <button className='bg-[#FFE01B] border border-black border-2 px-4 py-2 rounded-full'>
+          Login
+        </button>
+        <button>
+          <Menu />
+        </button>
       </div>
     </div>
   );
